7c0e2c34afcb54697bb14680819c481bbc8d5ddf
Yan Carlos <yancarlosmorejonfonseca@gmail.com>
2025-06-06 14:32:20 -0400

    agregar configuración de logging y mejorar manejo de errores en la extracción de datos

---
+++ scrapiando-mercado/main.py
@@ -0,0 +1,41 @@
+from spider import extraer_html,obtener_datos
+import logging
+import os
+from datetime import datetime
+import sys
+
+# Configuración del logging
+def setup_logging():
+    # Crear directorio de logs si no existe
+    log_dir = 'logs'
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
+    
+    # Configurar el formato y el archivo de log
+    log_file = f'logs/scraping_{datetime.now().strftime("%Y%m%d")}.log'
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s - %(levelname)s - %(message)s',
+        handlers=[
+            logging.FileHandler(log_file),
+            logging.StreamHandler(sys.stdout)
+        ]
+    )
+    
+#Datos necesarios para el programa
+URL = "https://listado.mercadolibre.com.ar/laptops#D[A:laptops]"
+
+def main():
+    setup_logging()
+    
+    html = extraer_html(URL)
+    if html:
+        logging.info("Empezando a extraer los datos")
+        datos = obtener_datos(html)
+        for name , price in zip(datos[0],datos[1]):
+            print(f"Nombre : {name.text}\nPrecio : {price.text}")
+    else:
+        logging.error("Error al empezar extraer los datos")
+
+if __name__ == main():
+    main()
--- scrapiando-mercado/spider.py
+++ scrapiando-mercado/spider.py
@@ -1,19 +1,25 @@
 from bs4 import BeautifulSoup
 import requests
 
+#Datos necesarios para realizar la petición get
+handlers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
+    "Accept-Language": "es-ES,es;q=0.9,en;q=0.8"
+}
+
 #Extraer html
 def extraer_html(URL):
     try:
-        respuesta = requests.get(URL)
+        respuesta = requests.get(URL, headers=handlers)
         respuesta.raise_for_status()
         return respuesta.content
     except Exception as e:
         print(f"Error al intentar realizar la petición : {e}")
         
 #Parsear html y obtener datos
-def obtener_html(html):
+def obtener_datos(html):
     sopa = BeautifulSoup(html,"html.parser")
     busqueda = sopa.find_all("div",{"class" : "poly-card__content"})[0:5]
     nombre =[dato.find("span",{"class" : "poly-component__brand"}) for dato in busqueda]
     precio = [dato.find("div",{"class" : "poly-price__current"}) for dato in busqueda]
-    return nombre.text,precio.text
+    return nombre,precio
