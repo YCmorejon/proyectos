3db5fadb10ea1985ff59eb62981a697752c11508
Yan Carlos <yancarlosmorejonfonseca@gmail.com>
2025-06-06 17:19:39 -0400

    agregando archivos iniciales para ela mejora del proyecto

---
+++ scrapiando-mercado/scrapiando-mercado-2.0/main.py
@@ -0,0 +1,1 @@
+from spider import extraer_html,obtener_datos
---
+++ scrapiando-mercado/scrapiando-mercado-2.0/spider.py
@@ -0,0 +1,11 @@
+import requests
+from bs4 import BeautifulSoup
+import logging
+
+#Extrar html
+def extraer_html():
+    pass
+
+#Parsear el html y obtener los datos persistentes
+def obtener_datos():
+    pass
