530507b3542718df97ff5e17267f99a644068f98
Yan Carlos <yancarlosmorejonfonseca@gmail.com>
2025-06-06 13:46:02 -0400

    mejorar función obtener_html para manejar datos de manera más robusta

--- scrapiando-mercado/spider.py
+++ scrapiando-mercado/spider.py
@@ -11,3 +11,9 @@ def extraer_html(URL):
         print(f"Error al intentar realizar la petición : {e}")
         
 #Parsear html y obtener datos
+def obtener_html(html):
+    sopa = BeautifulSoup(html,"html.parser")
+    busqueda = sopa.find_all("div",{"class" : "poly-card__content"})[0:5]
+    nombre =[dato.find("span",{"class" : "poly-component__brand"}) for dato in busqueda]
+    precio = [dato.find("div",{"class" : "poly-price__current"}) for dato in busqueda]
+    return nombre.text,precio.text
