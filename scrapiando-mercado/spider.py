bb7da66e66d5ef49a38bfb4f7bb6e72e251d5259
Yan Carlos <yancarlosmorejonfonseca@gmail.com>
2025-06-06 13:14:07 -0400

    creando el spider para el scraper del mercado

---
+++ scrapiando-mercado/spider.py
@@ -0,0 +1,11 @@
+from bs4 import BeautifulSoup
+import requests
+
+#Extraer html
+def extraer_html(URL):
+    try:
+        respuesta = requests.get(URL)
+        respuesta.raise_for_status()
+        return respuesta.content
+    except Exception as e:
+        print(f"Error al intentar realizar la petición : {e}")
