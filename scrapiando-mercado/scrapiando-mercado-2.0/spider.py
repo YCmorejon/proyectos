128109c276fe79b8e0800fd1926f9c97a744a400
Yan Carlos <yancarlosmorejonfonseca@gmail.com>
2025-06-06 17:28:45 -0400

    mejorando la función de extracción de HTML y actualizando la estructura de manejo de datos(incompleto aún)

--- scrapiando-mercado/scrapiando-mercado-2.0/spider.py
+++ scrapiando-mercado/scrapiando-mercado-2.0/spider.py
@@ -2,10 +2,27 @@ import requests
 from bs4 import BeautifulSoup
 import logging
 
-#Extrar html
-def extraer_html():
-    pass
+#Handlers
+handlers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36",
+    "Accept-Language": "es-ES,es;q=0.9,en;q=0.8"
+}
+
+#Extraer html
+def extraer_html(URL):
+    try:
+        respuesta = requests.get(URL, headers=handlers)
+        respuesta.raise_for_status()
+        return respuesta.content
+    except Exception as e:
+        logging.error(f"Ha ocurrido un error {e}")
 
 #Parsear el html y obtener los datos persistentes
-def obtener_datos():
-    pass
+def obtener_datos(html):
+    try:
+        sopa = BeautifulSoup(html,"html.parser")
+        busqueda = sopa.find_all("div",{"class" : "promo-text"})
+        articulos = ""
+        return articulos
+    except:
+        pass
