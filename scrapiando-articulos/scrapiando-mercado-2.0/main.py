0b8650305d646f33fd1902376975642c765ad3e7
Yan Carlos <yancarlosmorejonfonseca@gmail.com>
2025-06-07 11:59:22 -0400

    agregando el archivo main.py con la implementación inicial del scraper y la exportación de datos a CSV

---
+++ scrapiando-articulos/scrapiando-mercado-2.0/main.py
@@ -0,0 +1,11 @@
+from spider import Scraper
+import pandas as pd
+
+def main():
+    scraper = Scraper()
+    articulos = scraper.extraer_datos()
+    df = pd.DataFrame({"Articulos" : articulos})
+    df.to_csv("articulos.csv")
+    
+if __name__ == "__main__":
+    main()
